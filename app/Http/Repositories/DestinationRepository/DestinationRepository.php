d6a826dc5812222bc120aeafb04832770d752670
Quang Truong <nqtruong2510@gmail.com>
2022-07-18 04:08:49 +0700

    aaaa

--- app/Http/Repositories/DestinationRepository/DestinationRepository.php
+++ app/Http/Repositories/DestinationRepository/DestinationRepository.php
@@ -110,7 +110,7 @@ class DestinationRepository implements DestinationRepositoryInterface
     }
 
     public function deleteScheduleList(){
-        return Destination::where('user_id', JWTAuth::user()->id)->update([['is_schedule' => false],['is_complete' => false]]);
+        return Destination::where('user_id', JWTAuth::user()->id)->update(['is_schedule' => false],['is_complete' => false]);
     }
 
     public function submitCompleteSchedule($id){
